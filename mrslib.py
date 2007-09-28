811ce76003135a9e5f01318e01ca022381a95193
Andrew McNabb <amcnabb@mcnabbs.org>
2007-09-27 22:15:01 -0600

    added stuff for encoding/decoding data as hex

---
+++ mrslib.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from itertools import islice
+
+def enhex(byteseq):
+    """Encode an arbitrary byte sequence as an ASCII hexadecimal string.
+    
+    Make sure that whatever you send in is packed as a str.  Use the
+    struct module in the standard Python library to help you do this.
+    """
+    # Note that hex() returns strings like '0x61', and we don't want the 0x.
+    return ''.join(hex(ord(byte))[2:] for byte in byteseq)
+
+def dehex(hexstr):
+    """Decode a string of ASCII hexadecimal characters as a byte sequence.
+    
+    This will raise a ValueError if the input can't be interpreted as a string
+    of hexadecimal characters (e.g., if you have a 'q' in there somewhere).
+    By the way, you may wish to unpack the data.  Use the struct module to do
+    this.
+    """
+    return ''.join(chr(int(pair, 16)) for pair in group_by_two(hexstr))
+
+def group_by_two(s):
+    """Read a string two characters at a time.
+
+    If there's an odd number of characters, throw out the last one.
+    """
+    I = iter(s)
+    while True:
+        yield I.next() + I.next()
+
+
+# vim: et sw=4 sts=4
