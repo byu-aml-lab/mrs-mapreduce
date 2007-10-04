54ad00b505f7ab167d6abc294313f0522f60f230
Andrew McNabb <amcnabb@mcnabbs.org>
2007-10-04 02:30:37 -0600

    serial POSIX mapreduce works!

--- examples/wordcount.py
+++ examples/wordcount.py
@@ -7,17 +7,35 @@ def mapper(key, value):
 def reducer(key, value_iter):
     yield str(sum(int(x) for x in value_iter))
 
-if __name__ == '__main__':
+def wordcount_serial():
     from mrs.mapreduce import Operation, SerialJob
     import sys
     if len(sys.argv) != 2:
         print "usage: %s inputfile" % sys.argv[0]
         sys.exit(-1)
     filename = sys.argv[1]
-    op = Operation(mapper, reducer, output_dir='.')
-    mrsjob = SerialJob()
+    op = Operation(mapper, reducer)
+    mrsjob = SerialJob(output_dir='.')
     mrsjob.inputs.append(filename)
     mrsjob.operations.append(op)
     mrsjob.run()
+
+def wordcount_posix():
+    from mrs.mapreduce import Operation, POSIXJob
+    import sys
+    if len(sys.argv) < 2:
+        print "usage: %s inputfile1 [inputfile2 ...]" % sys.argv[0]
+        sys.exit(-1)
+    filenames = sys.argv[1:]
+    op = Operation(mapper, reducer, map_tasks=len(filenames),
+            reduce_tasks=2)
+    mrsjob = POSIXJob('/home/amcnabb/python/mrs/tmp')
+    mrsjob.inputs = filenames
+    mrsjob.operations.append(op)
+    mrsjob.run()
+
+if __name__ == '__main__':
+    #wordcount_serial()
+    wordcount_posix()
 
 # vim: et sw=4 sts=4
