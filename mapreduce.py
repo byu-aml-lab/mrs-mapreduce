2f33fff3aacf50e0243127c5c57f567214213c84
Andrew McNabb <amcnabb@mcnabbs.org>
2007-10-04 02:30:37 -0600

    serial POSIX mapreduce works!

--- mapreduce.py
+++ mapreduce.py
@@ -34,12 +34,18 @@ class Operation(object):
     
     The output_format is a file format, such as HexFile or TextFile.
     """
-    def __init__(self, mapper, reducer, output_format=None,
-            output_dir='.', output_prefix=None):
+    def __init__(self, mapper, reducer, map_tasks=1, reduce_tasks=1,
+            input_format=None, output_format=None, output_prefix=None):
         self.mapper = mapper
         self.reducer = reducer
+        self.map_tasks = map_tasks
+        self.reduce_tasks = reduce_tasks
+
+        if input_format is None:
+            self.input_format = textfile.TextFile
+        else:
+            self.input_format = input_format
         self.output_prefix = output_prefix
-        self.output_dir = output_dir
         if output_format is None:
             self.output_format = textfile.TextFile
         else:
@@ -76,8 +82,9 @@ class Job(object):
 class SerialJob(Job):
     """MapReduce execution on a single processor
     """
-    def __init__(self):
+    def __init__(self, output_dir):
         Job.__init__(self)
+        self.output_dir = output_dir
 
     def run(self, debug=False):
         """Run a MapReduce operation in serial.
@@ -96,19 +103,19 @@ class SerialJob(Job):
 
         # MAP PHASE
         input_file = textfile.TextFile(open(input))
-        fd, intermediate_name = tempfile.mkstemp(prefix='mrs.intermediate_')
-        intermediate_tmp = os.fdopen(fd, 'w')
-        intermediate_file = hexfile.HexFile(intermediate_tmp)
+        fd, interm_name = tempfile.mkstemp(prefix='mrs.interm_')
+        interm_tmp = os.fdopen(fd, 'w')
+        interm_file = hexfile.HexFile(interm_tmp)
 
-        map(operation.mapper, input_file, intermediate_file)
+        map(operation.mapper, input_file, interm_file)
 
         input_file.close()
-        intermediate_file.close()
+        interm_file.close()
 
         # SORT PHASE
         fd, sorted_name = tempfile.mkstemp(prefix='mrs.sorted_')
         os.close(fd)
-        hexfile.sort(intermediate_name, sorted_name)
+        hexfile.sort(interm_name, sorted_name)
 
         # REDUCE PHASE
         sorted_file = hexfile.HexFile(open(sorted_name))
@@ -116,7 +123,7 @@ class SerialJob(Job):
         if output_prefix is None:
             output_prefix = 'mrs.output_'
         fd, output_name = tempfile.mkstemp(prefix=output_prefix,
-                dir=operation.output_dir)
+                dir=self.output_dir)
         output_tmp = os.fdopen(fd, 'w')
         output_file = operation.output_format(output_tmp)
 
@@ -129,7 +136,7 @@ class SerialJob(Job):
 
         if not debug:
             import os
-            os.unlink(intermediate_name)
+            os.unlink(interm_name)
             os.unlink(sorted_name)
 
         return [output_name]
@@ -141,18 +148,111 @@ class POSIXJob(Job):
     Specify a directory located in shared storage which can be used as scratch
     space.
     """
-    def __init__(self, shared_dir):
+    def __init__(self, shared_dir, reduce_tasks=1):
         Job.__init__(self)
         self.shared_dir = shared_dir
+        self.partition = default_partition
 
-def map(mapper, input_file, output_file):
-    """Perform a map from the entries in input_file into output_file.
+    def run(self, debug=False):
+        import os
+        from tempfile import mkstemp, mkdtemp
+
+        if len(self.operations) != 1:
+            raise NotImplementedError("Requires exactly one operation.")
+        operation = self.operations[0]
+
+        map_tasks = operation.map_tasks
+        if map_tasks != len(self.inputs):
+            raise NotImplementedError("Requires exactly 1 map_task per input.")
+
+        reduce_tasks = operation.reduce_tasks
+
+        # PREP
+        jobdir = mkdtemp(prefix='mrs.job_', dir=self.shared_dir)
+
+        interm_path = os.path.join(jobdir, 'interm_')
+        interm_dirs = [interm_path + str(i) for i in xrange(reduce_tasks)]
+        for name in interm_dirs:
+            os.mkdir(name)
+
+        output_dir = os.path.join(jobdir, 'output')
+        os.mkdir(output_dir)
+
+
+        # MAP PHASE
+        ## still serial
+        for mapper_id, filename in enumerate(self.inputs):
+            input_file = operation.input_format(open(filename))
+            # create a new interm_name for each reducer
+            interm_filenames = [os.path.join(d, 'from_%s' % mapper_id)
+                    for d in interm_dirs]
+            interm_files = [hexfile.HexFile(open(name, 'w'))
+                    for name in interm_filenames]
+
+            map(operation.mapper, input_file, interm_files,
+                    partition=self.partition)
+
+            input_file.close()
+            for f in interm_files:
+                f.close()
+
+        for reducer_id in xrange(operation.reduce_tasks):
+            # SORT PHASE
+            interm_directory = interm_path + str(reducer_id)
+            fd, sorted_name = mkstemp(prefix='mrs.sorted_')
+            os.close(fd)
+            interm_filenames = [os.path.join(interm_directory, s)
+                    for s in os.listdir(interm_directory)]
+            hexfile.sort(interm_filenames, sorted_name)
+
+            # REDUCE PHASE
+            sorted_file = hexfile.HexFile(open(sorted_name))
+            output_prefix = operation.output_prefix
+            if output_prefix is None:
+                output_prefix = 'reducer_'
+            basename = output_prefix + str(reducer_id)
+            output_name = os.path.join(output_dir, basename)
+            output_file = operation.output_format(open(output_name, 'w'))
+
+            reduce(operation.reducer, sorted_file, output_file)
+
+            sorted_file.close()
+            output_file.close()
+
+        # CLEANUP
+
+        #if not debug:
+        #    import os
+        #    os.unlink(interm_name)
+        #    os.unlink(sorted_name)
+
+        #return [output_name]
+
+
+
+def default_partition(x, n):
+    return hash(x) % n
+
+def map(mapper, input_file, output_files, partition=None):
+    """Perform a map from the entries in input_file into output_files.
+
+    If partition is None, output_files should be a single file.  Otherwise,
+    output_files is a list, and partition is a function that takes a key and
+    returns the index of the file in output_files to which that key should be
+    written.
     """
+    if partition is not None:
+        N = len(output_files)
     while True:
         try:
             input = input_file.next()
-            for key, value in mapper(*input):
-                output_file.write(key, value)
+            if partition is None:
+                for key, value in mapper(*input):
+                    output_files.write(key, value)
+            else:
+                for key, value in mapper(*input):
+                    index = partition(key, N)
+                    output_files[index].write(key, value)
         except StopIteration:
             return
 
