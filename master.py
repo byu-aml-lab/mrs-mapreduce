d47bf7e70e1ff51623c6aafbc58982ff6901d319
Andrew McNabb <amcnabb@mcnabbs.org>
2007-10-11 12:22:47 -0600

    slave can connect in to server now

--- master.py
+++ master.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+class Slaves(object):
+    pass
 
 class MasterRPC(object):
     def _listMethods(self):
@@ -8,7 +10,6 @@ class MasterRPC(object):
     def signin(self, cookie, slave_port, host=None, port=None):
         """Slave reporting for duty.
         """
-        print 'host: %s, port: %s' % (host, port)
         return 4
 
     def ping(self):
@@ -16,6 +17,15 @@ class MasterRPC(object):
         """
         return True
 
+
+if __name__ == '__main__':
+    # Testing standalone server.
+    import rpc
+    instance = MasterRPC()
+    PORT = 8000
+    #PORT = 0
+    server = rpc.new_server(instance, host='127.0.0.1', port=PORT)
+    server.serve_forever()
 
 
 # vim: et sw=4 sts=4
