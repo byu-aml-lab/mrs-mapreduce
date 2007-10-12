be73123f08e0ddb39b54176d361a7c9a4b8a4e25
Andrew McNabb <amcnabb@mcnabbs.org>
2007-10-11 23:01:12 -0600

    started adding Slaves manager with locking, etc.

--- master.py
+++ master.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# TODO: Switch to using "with" for locks when we stop supporting pre-2.5.
+# from __future__ import with_statement
+
 import threading
 
 class Slave(object):
@@ -9,11 +12,64 @@ class Slave(object):
         self.cookie = cookie
 
 class Slaves(object):
-    pass
+    def __init__(self):
+        self._slaves = []
+        self._idle_slaves = []
+
+        self._lock = threading.Lock()
+        self._idle_sem = threading.Semaphore()
+
+    def add_slave(self, slave):
+        self._lock.acquire()
+        self._slaves.append(slave)
+        self._idle_slaves.append(slave)
+        self._lock.release()
+
+    del remove_slave(self, slave):
+        """Remove a slave, whether it is busy or idle.
+
+        Presumably, the slave has stopped responding.
+        """
+        self._lock.acquire()
+        if slave in self._idle_slaves:
+            self._idle_slaves.remove(slave)
+        self._slaves.remove(slave)
+        self._lock.release()
+
+    def push_idle(self, slave):
+        """Set a slave as idle.
+        """
+        self._lock.acquire()
+        if slave not in self._slaves:
+            self._lock.release()
+            raise RuntimeError("Slave does not exist!")
+        if slave not in self._idle_slaves:
+            self._idle_slaves.append(slave)
+        self._idle_sem.release()
+        self._lock.release()
+
+    def pop_idle(self, blocking=False):
+        """Request an idle slave, setting it as busy.
+
+        Return None if all slaves are busy.  Block if requested with the
+        blocking parameter.  If you set blocking, we will never return None.
+        """
+        idler = None
+        while blocking and idler is None:
+            if self._idle_sem.acquire(blocking):
+                self._lock.acquire()
+                try:
+                    idler = self._idle_slaves.pop()
+                except IndexError:
+                    # This can happen if remove_slave was called.  So sad.
+                    pass
+                self._lock.release()
+        return idler
+
 
 class MasterRPC(object):
     def __init__(self):
-        self.slaves = []
+        self.slaves = Slaves()
 
     def _listMethods(self):
         import SimpleXMLRPCServer
@@ -23,8 +79,13 @@ class MasterRPC(object):
         """Slave reporting for duty.
         """
         slave = Slave(host, slave_port, cookie)
-        self.slaves.append(slave)
+        self.slaves.add_slave(slave)
         return True
+
+    def done(self, cookie, host=None, port=None):
+        """Slave is done with whatever it was working on.
+        """
+        pass
 
     def ping(self):
         """Slave checking if we're still here.
