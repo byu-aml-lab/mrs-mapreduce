167e4ce6a491c81be98ed20cafe66387711b610d
Andrew McNabb <amcnabb@mcnabbs.org>
2007-10-12 01:46:43 -0600

    started moving out formats

---
+++ formats/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from textfile import TextFile
+from hexfile import HexFile, hexfile_sort
+
+format_map = {
+        '.txt': TextFile,
+        '.hexfile': HexFile,
+        }
+default_format = TextFile
+
+def fileformat(filename):
+    import os
+    extension = os.path.split_ext(filename)[1]
+    return format_map.get(extension, default_format)
+
+__all__ = ['TextFile', 'HexFile', 'hexfile_sort', 'fileformat']
+
+# vim: et sw=4 sts=4
