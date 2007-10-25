4f6531ece4ddb164dc1d9d8c181576c47ac67e1e
Andrew McNabb <amcnabb@mcnabbs.org>
2007-10-25 15:23:22 -0600

    The mockparallel implementation now works, at least with 1 reducer.

--- io/__init__.py
+++ io/__init__.py
@@ -37,23 +37,24 @@ def fileformat(filename):
     extension = os.path.splitext(filename)[1]
     return format_map.get(extension, default_format)
 
-def openfile(filename):
+def openfile(filename, mode='r'):
     """Open a file to be read as a sequence of key-value pairs.
     
     The file format is inferred from the filename.
     """
     input_format = fileformat(filename)
-    return input_format(open(filename))
+    return input_format(open(filename, mode))
 
 
 # TODO: Make Output cache output in memory if it's not so big that it needs
 # to be written to disk.
 class Output(object):
     """Manage output for a map or reduce task."""
-    def __init__(self, partition, splits, directory=None):
+    def __init__(self, partition, n, directory=None, format='hexfile'):
         self.partition = partition
-        self.splits = splits
-        self.buckets = [[] for i in xrange(n)]
+        self.n = n
+        self.splits = [([], None) for i in xrange(n)]
+        self.format = format
 
         if directory is None:
             from tempfile import mkdtemp
@@ -70,27 +71,39 @@ class Output(object):
 
     def collect(self, itr):
         """Collect all of the key-value pairs from the given iterator."""
-        if n == 1:
-            append = self.buckets[0].append
+        if self.n == 1:
+            bucket, filename = self.splits[0]
+            append = bucket.append
             for key, value in itr:
                 append((key, value))
+            filename = self.path(0)
+            self.splits[0] = bucket, filename
         else:
             for key, value in itr:
-                split = self.partition(key, self.splits)
-                self.buckets[split].append((key, value))
+                split = self.partition(key, self.n)
+                bucket, filename = self.splits[split]
+                bucket.append((key, value))
+                if filename is None:
+                    self.splits[split] = bucket, self.path(split)
 
     def path(self, index):
         """Return the path to the output split for the given index."""
-        filename = "split_%s.hexfile" % i
+        filename = "split_%s.%s" % (index, self.format)
+        import os
         return os.path.join(self.directory, filename)
+
+    def filenames(self):
+        return [filename for bucket, filename in self.splits]
 
     def savetodisk(self):
         """Write out all of the key-value pairs to files."""
-        for i, bucket in enumerate(self.buckets):
-            f = io.HexFile(open(self.path(i), 'w'))
-            for key, value in bucket:
-                f.write(key, value)
-            f.close()
+        for bucket, filename in self.splits:
+            assert(bool(bucket) == bool(filename))
+            if filename is not None:
+                f = openfile(filename, 'w')
+                for key, value in bucket:
+                    f.write(key, value)
+                f.close()
 
 __all__ = ['TextFile', 'HexFile', 'hexfile_sort', 'fileformat', 'openfile']
 
