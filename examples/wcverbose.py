5a54988bd68b630800ef6d7b7e3378c9e19311ee
Andrew McNabb <amcnabb@mcnabbs.org>
2007-10-31 14:43:28 -0600

    added yet another wc example

---
+++ examples/wcverbose.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# Copyright 2008 Brigham Young University
+#
+# This file is part of Mrs.
+#
+# Mrs is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# Mrs is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Mrs.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Inquiries regarding any further use of the Materials contained on this site,
+# please contact the Copyright Licensing Office, Brigham Young University,
+# 3760 HBLL, Provo, UT 84602, (801) 422-9339 or 422-3821, e-mail
+# copyright@byu.edu.
+
+from string import punctuation
+import mrs
+
+def mapper(key, value):
+    for word in value.split():
+        word = word.strip(punctuation).lower()
+        if word:
+            yield (word, str(1))
+
+def reducer(key, value_iter):
+    yield str(sum(int(x) for x in value_iter))
+
+# options to be passed in (?):
+#  - default # of map tasks
+#  - default # of reduce tasks
+#  - input
+#  - output
+def program(input):
+    # TODO: introduce splitting mechanisms (i.e. mrs.Output(mrs.Map()))
+    data1 = mrs.Map(mapper, input)
+    output = mrs.Reduce(reducer, data1)
+    return output
+
+if __name__ == '__main__':
+    mrs.main(program)
+
+# vim: et sw=4 sts=4
