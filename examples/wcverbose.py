a21560d30dde4be5e28a726b6d9b700b0bd7c1cb
Andrew McNabb <amcnabb@mcnabbs.org>
2007-10-31 14:43:28 -0600

    added yet another wc example

---
+++ examples/wcverbose.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from string import punctuation
+import mrs
+
+def mapper(key, value):
+    for word in value.split():
+        word = word.strip(punctuation).lower()
+        if word:
+            yield (word, str(1))
+
+def reducer(key, value_iter):
+    yield str(sum(int(x) for x in value_iter))
+
+# options to be passed in (?):
+#  - default # of map tasks
+#  - default # of reduce tasks
+#  - input
+#  - output
+def program(input):
+    # TODO: introduce splitting mechanisms (i.e. mrs.Output(mrs.Map()))
+    data1 = mrs.Map(mapper, input)
+    output = mrs.Reduce(reducer, data1)
+    return output
+
+if __name__ == '__main__':
+    mrs.main(program)
+
+# vim: et sw=4 sts=4
