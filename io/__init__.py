c5f1522a6ee40249b29c65ed494838edaf607433
Andrew McNabb <amcnabb@mcnabbs.org>
2008-01-28 08:21:41 -0700

    replaced openfile with openbuf, for better concurrency

--- io/__init__.py
+++ io/__init__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
 __all__ = ['Buffer', 'TextFormat', 'HexFormat', 'hexformat_sort',
-        'fileformat', 'openfile']
+        'fileformat', 'openbuf']
 
 from textformat import TextFormat
 from hexformat import HexFormat, hexformat_sort
+from buffer import Buffer
 
 format_map = {
         '.txt': TextFormat,
@@ -12,26 +13,29 @@ format_map = {
         }
 default_format = TextFormat
 
+# TODO: Find a better way to infer the file format.
 def fileformat(filename):
     """Guess the file format according to extension of the given filename."""
     import os
     extension = os.path.splitext(filename)[1]
     return format_map.get(extension, default_format)
 
-# TODO: Find a better way to infer the file format.
-def openfile(url, mode='r'):
-    """Open a url or file to be read or written as a list of key-value pairs.
+def openbuf(url, mode='r'):
+    """Open a url or file into a Mrs Buffer
     
-    The file format is inferred from the filename.
+    Initially, the Buffers will be empty.  However, when Twisted's
+    reactor.run() is called, data will be read into all Buffers
+    simultaneously.
     """
     import urlparse, urllib2
     parsed_url = urlparse.urlparse(url, 'file')
     if parsed_url.scheme == 'file':
         f = open(parsed_url.path, mode)
+        buf = Buffer(filelike=f)
     else:
-        f = urllib2.urlopen(url)
-    input_format = fileformat(url)
-    return input_format(f)
+        from net import download
+        buf = download(url)
+    return buf
 
 
 # vim: et sw=4 sts=4
