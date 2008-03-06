6a1f0f233db46ea91737fa322c0ab91d72085be8
Andrew McNabb <amcnabb@mcnabbs.org>
2008-03-06 08:21:23 -0700

    split off mrs_map and mrs_reduce from task.py into mapreduce.py

---
+++ mapreduce.py
@@ -0,0 +1,68 @@
+# Mrs
+# Copyright 2008 Andrew McNabb <amcnabb-mrs@mcnabbs.org>
+#
+# This file is part of Mrs.
+#
+# Mrs is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# Mrs is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+# more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Mrs.  If not, see <http://www.gnu.org/licenses/>.
+
+
+def mrs_map(mapper, input):
+    """Perform a map from the entries in input."""
+    for inkey, invalue in input:
+        for key, value in mapper(inkey, invalue):
+            yield (key, value)
+
+def grouped_read(input_file):
+    """An iterator that yields key-iterator pairs over a sorted input_file.
+
+    This is very similar to itertools.groupby, except that we assume that the
+    input_file is sorted, and we assume key-value pairs.
+    """
+    input_itr = iter(input_file)
+    input = input_itr.next()
+    next_pair = list(input)
+
+    def subiterator():
+        # Closure warning: don't rebind next_pair anywhere in this function
+        group_key, value = next_pair
+
+        while True:
+            yield value
+            try:
+                input = input_itr.next()
+            except StopIteration:
+                next_pair[0] = None
+                return
+            key, value = input
+            if key != group_key:
+                # A new key has appeared.
+                next_pair[:] = key, value
+                return
+
+    while next_pair[0] is not None:
+        yield next_pair[0], subiterator()
+    raise StopIteration
+
+
+def mrs_reduce(reducer, input):
+    """Perform a reduce from the entries in input into output.
+
+    A reducer is an iterator taking a key and an iterator over values for that
+    key.  It yields values for that key.
+    """
+    for key, iterator in grouped_read(input):
+        for value in reducer(key, iterator):
+            yield (key, value)
+
+# vim: et sw=4 sts=4
