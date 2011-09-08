56b36f1684e66c923ccdb3c7adf2f42e978452d0
Dan Hogue <chmod740@gmail.com>
2011-09-08 08:36:59 -0600

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name="MapReduce Simplified",
+      version="0.5",
+      description="Mrs: A simplified MapReduce implimentation in Python",
+      long_description="See README",
+      license="GNU GPL",
+      author="BYU AML Lab",
+      author_email="kseppi@byu.edu",
+      url="http://aml.cs.byu.edu/",
+      packages=['mrs'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Operating System :: POSIX :: Linux',
+                   'Environment :: Console',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Natural Language :: English',
+                   'Programming Language :: Python',
+                   'Topic :: Scientific/Engineering :: Information Analysis'],
+     )
