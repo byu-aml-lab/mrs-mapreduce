ed371263c5f26586a747dcb6dd6e12576e004abc
Dan Hogue <chmod740@gmail.com>
2011-10-13 11:42:28 -0600

    Added new test files and simplified some stuff in runscript.py

--- examples/runscript.py
+++ examples/runscript.py
@@ -3,7 +3,7 @@
 """Runs a Mrs program using screen and pssh.
 
     Example:
-    $ python runscript.py --hosts yukon wordcount.py WordCount.java out
+    $ python runscript.py --hosts slaves wordcount.py test.txt
 """
 
 import getpass
@@ -14,54 +14,50 @@ import subprocess
 import sys
 import time
 
-DEFAULT_SCRATCH_DIR = '/'.join(('/aml/scratch', getpass.getuser()))
+DEFAULT_SCRATCH_DIR = os.getcwd()
 DEFAULT_LOCAL_SCRATCH = getpass.getuser()
 DEFAULT_JOBNAME = 'newjob'
 
 ##############################################################################
 # Setup
 
-parser = optparse.OptionParser(conflict_handler='resolve')
-parser.disable_interspersed_args()
+parser = optparse.OptionParser()
 
 parser.add_option('--hosts', 
         dest='hostfiles', 
         action='append',
-        help='hosts file (each line "[user@]host[:port]")', 
+        help='Hosts file (each line "[user]@[hostname]:[port]")', 
         default=[])
 parser.add_option('--jobname', 
         dest='jobname', 
         action='store',
-        help='job name', 
+        help='Job name. Default is \'newjob\'', 
         default=DEFAULT_JOBNAME)
 parser.add_option('--scratch', 
         dest='scratch_dir', 
         action='store',
-        help='scratch directory', 
+        help='Scratch directory. Default is current working directory', 
         default=DEFAULT_SCRATCH_DIR)
 parser.add_option('--local-scratch', 
         dest='local_scratch', 
         action='store',
-        help='local scratch (subdir of /net/$hostname, empty string to skip)',
+        help='Local scratch (subdir of /net/$hostname, empty string to skip)',
         default=DEFAULT_LOCAL_SCRATCH)
         
 opts, args = parser.parse_args()
-
-mrs_program = args[0]
-mrs_argv = args[1:]
 
 if not opts.hostfiles:
     print >>sys.stderr,'No hosts file specified!'
     sys.exit(1)
 
-if opts.jobname:
-    outdir = os.path.join(opts.scratch_dir, opts.jobname)
-    local_shared = os.path.join(opts.local_scratch, opts.jobname)
-else:
-    outdir = os.path.join(opts.scratch_dir, mrs_program)
-    local_shared = os.path.join(opts.local_scratch, mrs_program)
-    
-runfilename = os.path.join(outdir, 'master.run')
+# set some variables
+mrs_program = args[0]
+mrs_argv = args[1:]
+job_dir = os.path.join(opts.scratch_dir, opts.jobname)
+local_shared = os.path.join(opts.local_scratch, opts.jobname)
+runfilename = os.path.join(job_dir, 'master.run')
+host_options = ' '.join('--hosts %s' % hostfile for hostfile in opts.hostfiles)
+master_hostname = socket.gethostname()
 
 # This method is called to run commands on the command line
 def run(*args):
@@ -74,35 +70,28 @@ def run(*args):
 # Start Master
 
 MASTER_COMMAND = ' '.join((
-    'python %s' % mrs_program,
-    ' '.join(mrs_argv),
+    'python %s' % mrs_program, ' '.join(mrs_argv), '%s/results' % job_dir,
     '--mrs=Master',
     '--mrs-verbose',
     '--mrs-runfile %s' % runfilename,
-    '--mrs-shared %s' % outdir,
-    '2>%s/master.err' % outdir,
-    '|tee %s/master.out' % outdir))
-
-STDERR_COMMAND = 'tail -F %s/master.err' % outdir
+    '--mrs-shared %s' % job_dir,
+    '2>%s/master.err' % job_dir,
+    '|tee %s/master.out' % job_dir))
 
 # Make the job directory (note that this will fail if it already exists).
 try:
-    os.makedirs(outdir)
+    os.makedirs(job_dir)
 except:
-    print >>sys.stderr,'Error, job directory \"%s\" may already exist!' % outdir
+    print >>sys.stderr,'Error, job directory \"%s\" may already exist!' % job_dir
     sys.exit(1)
 
 # Create a screen session named after the job name.
 run('screen', '-dmS', opts.jobname)
-
-# Add a second and third window in the screen session.
-run('screen', '-S', opts.jobname, '-X', 'screen')
 run('screen', '-S', opts.jobname, '-X', 'screen')
 
 print 'Starting the master.'
 run('screen', '-S', opts.jobname, '-p0', '-X', 'stuff', MASTER_COMMAND + '\n')
 
-print 'Waiting for the master to start up.'
 while True:
     try:
         runfile = open(runfilename)
@@ -113,17 +102,11 @@ while True:
 master_port = runfile.read().strip()
 runfile.close()
 
-run('screen', '-S', opts.jobname, '-p1', '-X', 'stuff', STDERR_COMMAND + '\n')
-
 ##############################################################################
 # Start Slaves
 
-host_options = ' '.join('-h %s' % hostfile for hostfile in opts.hostfiles)
-pwd = os.getcwd()
-master_hostname = socket.gethostname()
-
 SLAVE_COMMAND = ' '.join((
-    'cd %s;' % pwd,
+    'cd %s;' % os.getcwd(),
     'python %s' % mrs_program,
     '--mrs=Slave',
     '--mrs-verbose',
@@ -132,16 +115,16 @@ SLAVE_COMMAND = ' '.join((
     
 PSSH_COMMAND = ' '.join((
     'pssh', host_options,
-    '-o', '%s/slaves.out' % outdir, 
-    '-e', '%s/slaves.err' % outdir,
+    '-o', '%s/slaves.out' % job_dir, 
+    '-e', '%s/slaves.err' % job_dir,
     '-t -1 -p 1000',
     '"%s"' % SLAVE_COMMAND))
 
 print 'Starting the slaves.'
-run('screen', '-S', opts.jobname, '-p2', '-X', 'stuff', PSSH_COMMAND + '\n')
+run('screen', '-S', opts.jobname, '-p1', '-X', 'stuff', PSSH_COMMAND + '\n')
 
 # Load the screen session.
-print 'Connecting.'
+print 'Loading screen session'
 run('screen', '-r', opts.jobname, '-p0')
 
 
