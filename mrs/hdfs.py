ac40e0eff7c7948145204d4f5578c578c66f38d1
Andrew McNabb <amcnabb@mcnabbs.org>
2012-09-01 17:34:17 -0600

    Implement most of the rest of the webhdfs protocol (testing still needed)

--- mrs/hdfs.py
+++ mrs/hdfs.py
@@ -20,7 +20,11 @@
 # Licensing Office, Brigham Young University, 3760 HBLL, Provo, UT 84602,
 # (801) 422-9339 or 422-3821, e-mail copyright@byu.edu.
 
-"""Hadoop Distributed File System WebHDFS Client"""
+"""Hadoop Distributed File System WebHDFS Client
+
+The protocol specification is defined at:
+    http://hadoop.apache.org/common/docs/r1.0.0/webhdfs.html
+"""
 
 from __future__ import division
 
@@ -40,14 +44,53 @@ class WebHDFSProxy(object):
         self._homedir = None
         self._username = username
 
+    ##########################################################################
+    # Get Methods
+
+    def open(self, path, **args):
+        """Read a file.
+
+        If the path is not absolute (starting with '/'), then it's relative to
+        the user's home directory.
+        """
+        if not path.startswith('/'):
+            path = os.path.join(self.get_home_directory(), path)
+        response = self._namenode_request('GET', path, 'OPEN', args)
+        response.read()
+        check_code(response.status, content, httplib.TEMPORARY_REDIRECT)
+        datanode_url = response.getheader('Location')
+
+        response = self._datanode_get(datanode_url)
+        content = response.read()
+        check_code(response.status, content)
+        return content
+
     def get_home_directory(self):
         """Returns the path to the home directory of the configured user."""
         if self._homedir is None:
-            response = self._namenode_get('/', 'GETHOMEDIRECTORY')
-            path_json = response.read()
-            path_object = json.loads(path_json)
-            self._homedir = path_object['Path']
+            response = self._namenode_request('GET', '/', 'GETHOMEDIRECTORY')
+            content = response.read()
+            check_code(response.status, content)
+            path_json = json.loads(content)
+            self._homedir = path_json['Path']
         return self._homedir
+
+    def get_file_status(self, path):
+        """List a directory.
+
+        Returns a dictionaries which contains the keys "accessTime",
+        "blockSize", "group", "length", "modificationTime", "owner",
+        "pathSuffix", "permission", "replication", and "type".  If the path is
+        not absolute (starting with '/'), then it's relative to the user's
+        home directory.
+        """
+        if not path.startswith('/'):
+            path = os.path.join(self.get_home_directory(), path)
+        response = self._namenode_request('GET', path, 'GETFILESTATUS')
+        content = response.read()
+        check_code(response.status, content)
+        filestatuses_json = json.loads(content)
+        return filestatuses_json['FileStatus']
 
     def list_status(self, path):
         """List a directory.
@@ -58,21 +101,172 @@ class WebHDFSProxy(object):
         "replication", and "type".  If the path is not absolute (starting with
         '/'), then it's relative to the user's home directory.
         """
-        response = self._namenode_get(path, 'LISTSTATUS')
-        filestatuses_json = response.read()
-        filestatuses_object = json.loads(filestatuses_json)
-        return filestatuses_object['FileStatuses']['FileStatus']
+        if not path.startswith('/'):
+            path = os.path.join(self.get_home_directory(), path)
+        response = self._namenode_request('GET', path, 'LISTSTATUS')
+        content = response.read()
+        check_code(response.status, content)
+        filestatuses_json = json.loads(content)
+        return filestatuses_json['FileStatuses']['FileStatus']
 
-    def _namenode_get(self, path, op, args=None):
-        """Send a GET request to the namenode.
+    def get_contents_summary(self, path):
+        """Get content summary of a directory.
+
+        Returns a dictionary containing the keys "directoryCount",
+        "fileCount", "length", "quota", "spaceConsumed", and "spaceQuota".  If
+        the path is not absolute (starting with '/'), then it's relative to
+        the user's home directory.
+        """
+        if not path.startswith('/'):
+            path = os.path.join(self.get_home_directory(), path)
+        response = self._namenode_request('GET', path, 'LISTSTATUS')
+        content = response.read()
+        check_code(response.status, content)
+        filestatuses_json = json.loads(content)
+        return filestatuses_json['FileStatuses']['FileStatus']
+
+    ##########################################################################
+    # Put/Delete Methods
+
+    # Unlike the other commands, CREATE and APPEND require a two-step process
+    # to ensure that data is not unnecessarily sent to the namenode.
+
+    def create(self, path, data, **args):
+        """Create and write to a file.
+
+        The `data` parameter can be either a string or a file (but not
+        necessarily a filelike in general--see the httplib docs).  If the path
+        is not absolute (starting with '/'), then it's relative to the user's
+        home directory.
+        """
+        if not path.startswith('/'):
+            path = os.path.join(self.get_home_directory(), path)
+        response = self._namenode_request('PUT', path, 'CREATE', args)
+        response.read()
+        check_code(response.status, content, httplib.TEMPORARY_REDIRECT)
+        datanode_url = response.getheader('Location')
+
+        response = self._datanode_request('PUT', datanode_url, data)
+        content = response.read()
+        check_code(response.status, content, httplib.CREATED)
+
+    def append(self, path, data, **args):
+        """Append to a file.
+
+        Note that the HDFS server may or may not support appending.  The
+        `data` parameter can be either a string or a file (but not necessarily
+        a filelike in general--see the httplib docs).  If the path is not
+        absolute (starting with '/'), then it's relative to the user's home
+        directory.
+        """
+        if not path.startswith('/'):
+            path = os.path.join(self.get_home_directory(), path)
+        response = self._namenode_request('PUT', path, 'APPEND', args)
+        response.read()
+        check_code(response.status, content, httplib.TEMPORARY_REDIRECT)
+        datanode_url = response.getheader('Location')
+
+        response = self._datanode_request('PUT', datanode_url, data)
+        content = response.read()
+        check_code(response.status, content, httplib.OK)
+
+    def mkdirs(self, path, **args):
+        """Make a directory.
+
+        If the path is not absolute (starting with '/'), then it's relative to
+        the user's home directory.
+        """
+        if not path.startswith('/'):
+            path = os.path.join(self.get_home_directory(), path)
+        response = self._namenode_request('PUT', path, 'MKDIRS', args)
+        content = response.read()
+        check_code(response.status, content)
+        boolean_json = json.loads(content)
+        return boolean_json['boolean']
+
+    def rename(self, path1, path2):
+        """Rename a file or directory.
+
+        If the path is not absolute (starting with '/'), then it's relative to
+        the user's home directory.
+        """
+        if not path1.startswith('/'):
+            path1 = os.path.join(self.get_home_directory(), path1)
+        if not path2.startswith('/'):
+            path2 = os.path.join(self.get_home_directory(), path2)
+        response = self._namenode_request('PUT', path, 'RENAME',
+                {'destination': path2})
+        content = response.read()
+        check_code(response.status, content)
+        boolean_json = json.loads(content)
+        return boolean_json['boolean']
+
+    def delete(self, path, **args):
+        """Make a directory.
+
+        If the path is not absolute (starting with '/'), then it's relative to
+        the user's home directory.
+        """
+        if not path.startswith('/'):
+            path = os.path.join(self.get_home_directory(), path)
+        response = self._namenode_request('DELETE', path, 'DELETE', args)
+        content = response.read()
+        check_code(response.status, content)
+        boolean_json = json.loads(content)
+        return boolean_json['boolean']
+
+    def set_permission(self, path, **args):
+        """Set permissions of a file or directory.
+
+        If the path is not absolute (starting with '/'), then it's relative to
+        the user's home directory.
+        """
+        if not path.startswith('/'):
+            path = os.path.join(self.get_home_directory(), path)
+        response = self._namenode_request('PUT', path, 'SETPERMISSION', args)
+        content = response.read()
+        check_code(response.status, content)
+
+    def set_permission(self, path, **args):
+        """Set permissions of a file or directory.
+
+        If the path is not absolute (starting with '/'), then it's relative to
+        the user's home directory.
+        """
+        if not path.startswith('/'):
+            path = os.path.join(self.get_home_directory(), path)
+        response = self._namenode_request('PUT', path, 'SETOWNER', args)
+        content = response.read()
+        check_code(response.status, content)
+
+    ##########################################################################
+    # Other
+
+    def _namenode_request(self, method, path, op, args=None, body=None):
+        """Send a PUT request to the namenode.
 
         Returns the HTTPResponse object, which is filelike. Note that this
         response object must be fully read before beginning to read any
         subsequent response.
         """
         request_uri = self._request_uri(path, op, args)
-        self._namenode_conn.request('GET', request_uri)
+        self._namenode_conn.request(method, request_uri, body)
         response = self._namenode_conn.getresponse()
+        return response
+
+    def _datanode_request(self, url, body=None):
+        """Send a PUT request to the datanode.
+
+        Returns the HTTPResponse object, which is filelike. Note that this
+        response object must be fully read before beginning to read any
+        subsequent response.
+        """
+        host = urlparse.urlparse(url)[1]
+        datanode_conn = httplib.HTTPConnection(host)
+        # TODO: check whether the datanode is happy with an absolute URL or
+        # if it needs a relative url here.
+        self._namenode_conn.request(method, url, body)
+        response = datanode_conn.getresponse()
         return response
 
     def _request_uri(self, path, op, args=None):
@@ -93,5 +287,38 @@ class WebHDFSProxy(object):
         request_uri = '%s?%s' % (quoted_path, quoted_query)
         return request_uri
 
+
+# Exceptions defined by the webhdfs spec. Note that IllegalArgumentException
+# and UnsupportedOperationException are combined.
+class IllegalArgumentException(Exception):
+    pass
+
+class SecurityException(Exception):
+    pass
+
+class IOException(Exception):
+    pass
+
+class FileNotFoundException(Exception):
+    pass
+
+exceptions = {400: IllegalArgumentException,
+        401: SecurityException,
+        403: IOException,
+        404: FileNotFoundException}
+
+def check_code(code, content, expected_code=httplib.OK):
+    """Raise a remote exception if necessary."""
+    if code == expected_code:
+        return
+
+    try:
+        exception_cls = exceptions[code]
+    except KeyError:
+        raise RuntimeError('Unknown webhdfs error code: %s' % code)
+
+    exception_json = json.loads(content)
+    message = exception_json['RemoteException']['message']
+    raise exception_cls(message)
 
 # vim: et sw=4 sts=4
