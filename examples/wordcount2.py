135fa648f1addeb98431ff7d282c60839bb7a4fa
Andrew McNabb <amcnabb@mcnabbs.org>
2012-09-11 13:54:25 -0600

    Update the wordcount examples

--- examples/wordcount2.py
+++ examples/wordcount2.py
@@ -21,19 +21,20 @@
 # Licensing Office, Brigham Young University, 3760 HBLL, Provo, UT 84602,
 # (801) 422-9339 or 422-3821, e-mail copyright@byu.edu.
 
-################################################################################
-#
-# wordcount2.py is just like wordcount.py except that it allows you to pass in
-# a single file that enumerates a set of input text files. (See the examples
-# folder for an example input file.) This is done by overriding the input_data()
-# function in MRS_HOME/mrs/mapreduce.py.
-#
-################################################################################
-
 import string
 import mrs
 
 class WordCount(mrs.MapReduce):
+    """Count the number of occurrences of each word in a set of documents.
+
+    This program is just like wordcount.py except:
+
+    1. It takes a single file that enumerates a set of input text files.  This
+    is done by overriding the input_data() function in
+    MRS_HOME/mrs/mapreduce.py.
+
+    2. It is smart about punctuation, case, etc.
+    """
 
     def map(self, key, value):
         for word in value.split():
@@ -42,16 +43,18 @@ class WordCount(mrs.MapReduce):
                 yield (word, str(1))
 
     def reduce(self, key, values):
-        yield str(sum(int(x) for x in values))
+        count = sum(int(x) for x in values)
+        yield str(count)
 
     def input_data(self, job):
         if len(self.args) < 2:
             print >>sys.stderr, "Requires input(s) and an output."
             return None
         inputs = []
-        f = open(self.args[0])
-        for line in f:
-            inputs.append(line[:-1])
+        for filename in self.args[:-1]:
+            with open(filename) as f:
+                for line in f:
+                    inputs.append(line.strip())
         return job.file_data(inputs)
 
 if __name__ == '__main__':
