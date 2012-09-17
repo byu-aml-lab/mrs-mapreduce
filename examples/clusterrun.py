5e06fbf69037c7de3e76e09ebb33b6b2a617e081
Andrew McNabb <amcnabb@mcnabbs.org>
2012-09-17 17:22:43 -0600

    Fix a race condition in clusterrun causing time.txt to get lost

--- examples/clusterrun.py
+++ examples/clusterrun.py
@@ -270,10 +270,19 @@ if not opts.background:
 # wait for the run file to be empty, then exit screen
 print('Mrs is running, feel free to control-c to quit')
 while True:
-    time.sleep(1)
+    time.sleep(0.5)
     try:
         with open(runfilename) as runfile:
             if runfile.read().strip() == '-':
+                break
+    except IOError:
+        break
+
+while True:
+    time.sleep(0.1)
+    try:
+        with open(timefilename) as timefile:
+            if timefile.read().strip():
                 break
     except IOError:
         break
