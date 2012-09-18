2765660bb3f2cdee4b54718372f402acb5f6eebf
Andrew McNabb <amcnabb@mcnabbs.org>
2012-09-18 10:04:45 -0600

    Fix another bug in clusterrun causing a premature quit

--- examples/clusterrun.py
+++ examples/clusterrun.py
@@ -270,21 +270,21 @@ if not opts.background:
 # wait for the run file to be empty, then exit screen
 print('Mrs is running, feel free to control-c to quit')
 while True:
-    time.sleep(0.5)
     try:
         with open(runfilename) as runfile:
             if runfile.read().strip() == '-':
                 break
     except IOError:
-        break
+        pass
+    time.sleep(0.5)
 
 while True:
-    time.sleep(0.1)
     try:
         with open(timefilename) as timefile:
             if timefile.read().strip():
                 break
     except IOError:
-        break
+        pass
+    time.sleep(0.1)
 
 quit_screen(opts.jobname)
