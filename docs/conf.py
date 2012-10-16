61f33c17a58c05f60eeacccc8c1ca8a99882c465
Andrew McNabb <amcnabb@mcnabbs.org>
2012-10-16 17:19:23 -0600

    Add documentation for building the documentation

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ source_suffix = '.txt'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Mrs-MapReduce'
+project = u'Mrs MapReduce'
 copyright = u'2012, AML Lab'
 
 # The version info for the project you're documenting, acts as replacement for
