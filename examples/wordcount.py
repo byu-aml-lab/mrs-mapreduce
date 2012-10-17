1be01d49e059f2065f9a5d0706c2173bf4257310
Andrew McNabb <amcnabb@mcnabbs.org>
2012-10-17 14:19:02 -0600

    Change from the GPL to the Apache Software License

--- examples/wordcount.py
+++ examples/wordcount.py
@@ -2,24 +2,17 @@
 # Mrs
 # Copyright 2008-2012 Brigham Young University
 #
-# This file is part of Mrs.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Mrs is free software: you can redistribute it and/or modify it under the
-# terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 3 of the License, or (at your option) any later
-# version.
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# Mrs is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-# details.
-#
-# You should have received a copy of the GNU General Public License along with
-# Mrs.  If not, see <http://www.gnu.org/licenses/>.
-#
-# Inquiries regarding any further use of Mrs, please contact the Copyright
-# Licensing Office, Brigham Young University, 3760 HBLL, Provo, UT 84602,
-# (801) 422-9339 or 422-3821, e-mail copyright@byu.edu.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import mrs
 import string
